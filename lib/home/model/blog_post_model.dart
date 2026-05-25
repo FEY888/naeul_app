96a7e598bc6cb7db92adb8880dc52dfb99505023
FEY <fey@FEYui-MacBookAir.local>
2026-05-26 01:07:24 +0900

    feat(home): 네이버 블로그 연동을 위한 BlogPostModel 도메인 구현 추후 const를 생정자에 붙여 메모리 재사용성 확보

---
+++ lib/home/model/blog_post_model.dart
@@ -0,0 +1,17 @@
+
+
+class BlogPostModel {
+  final String id;
+  final String title;
+  final String thumbnailUrl;
+  final String date;
+  final String url;
+
+  BlogPostModel({
+    required this.id,
+    required this.title,
+    required this.thumbnailUrl,
+    required this.date,
+    required this.url,
+});
+}
