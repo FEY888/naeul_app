95017d65f2ed2498955db9c2066b2e0ff46a2dfc
FEY <fey@FEYui-MacBookAir.local>
2026-05-26 00:57:57 +0900

    feat(router): StatefulShellRoute 기반 예약 내비게이션 구조 설계
    - 홈 및 예약 탭 간의 내비게이션 상태 유지를 위해 StatefulShellRoute.indexedStack 사용
    
    - 예약 단계에서 하단 탭바가 유지되는 현상은 사용자의 유연한 UX 이동을 위해 의도한 설계임

--- lib/common/provider/go_router.dart
+++ lib/common/provider/go_router.dart
@@ -1,9 +1,21 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:naeul_app/common/layout/default_layout.dart';
-import 'package:naeul_app/screen/home_screen.dart';
+import 'package:naeul_app/reservation/view/reservation_screen.dart';
+import 'package:naeul_app/screen/view/home_screen.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final routerProvider = Provider<GoRouter>((ref) {
+import '../../reservation/view/confirm_screen.dart';
+import '../../reservation/view/date_select_screen.dart';
+import '../../reservation/view/success_screen.dart';
+
+
+part 'go_router.g.dart';
+
+@Riverpod(
+  keepAlive: true
+)
+GoRouter router(Ref ref) {
   return GoRouter(
     initialLocation: '/home',
     routes: [
@@ -23,8 +35,30 @@ final routerProvider = Provider<GoRouter>((ref) {
               ),
             ],
           ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: '/reservation',
+                builder: (context, state) => const ReservationScreen(),
+                routes: [
+                  GoRoute(
+                    path: 'date',
+                    builder: (context, state) => const DateSelectScreen(),
+                  ),
+                  GoRoute(
+                    path: 'confirm',
+                    builder: (context, state) => const ConfirmScreen(),
+                  ),
+                  GoRoute(
+                    path: 'success',
+                    builder: (context, state) => const SuccessScreen(),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     ],
   );
-});
+}
---
+++ lib/common/provider/go_router.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'go_router.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$routerHash() => r'071ad01fe5f97419d94a239cc2582f11cd5e2781';
+
+/// See also [router].
+@ProviderFor(router)
+final routerProvider = Provider<GoRouter>.internal(
+  router,
+  name: r'routerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$routerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RouterRef = ProviderRef<GoRouter>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
